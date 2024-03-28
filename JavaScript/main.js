e4aac73a05f7dc997b66f412210986e6eda4303b
Felipe Cerqueira <felipecer06@gmail.com>
2024-03-28 13:16:10 -0300

    aplicacao de melhoria - customização de texto

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,6 +2,10 @@
 var arrayDeTipos = ['nome','nomeCompleto','cpf', 'email', 'telefone', 'celular', 'cep', 'endereco', 'complemento']
 var arrayDeCamposInvalidos = []
 var arrayDeCamposValidos = []
+var Regras = {}
+var index = 0
+var timeStamp
+
 
 //eventos
 $(document).ready(function() {
@@ -164,13 +168,20 @@ function reveladorDeMensagem(bool, objeto){
 }
 
 function mensagemErro(objeto){
+    var timer = new Date()
+    if (timeStamp == '' || timeStamp == undefined){
+        timeStamp = timer.getSeconds()
+    }
+
     var nodoPai = objeto.parent()
 
     if (document.querySelector(`#${nodoPai[0].id} #validation-message`) != null){
+        nodoPai.find('#validation-message').text(definirMensagemUsada(objeto))
         nodoPai.find('#validation-message').removeClass('hidden')
+        
     }
     else{
-        $(`<div id="validation-message">Por favor, preencha este campo.</div>`).appendTo(nodoPai)
+        $(`<div id="validation-message">${definirMensagemUsada(objeto)}</div>`).appendTo(nodoPai)
     }
     
 }
@@ -205,31 +216,156 @@ function verificadorDeCamposPreenchidos(bool=false){
         }
     }
     bloqueadorDeSubmit()
-    console.log(arrayDeCamposValidos)
-    console.log(arrayDeCamposInvalidos)
 }
+
+function definirMensagemUsada(objeto){
+    var timer = new Date
+    var timerCompardo = timer.getSeconds()
+
+    var tipo = objeto[0].id
+    switch (tipo) {
+        case 'nome':
+            var mensagem = ['mensagem 1', 'mensagem 2', 'mensagem 3', 'mensagem 4', 'mensagem 5']
+            
+            if (timeStamp <= timerCompardo - 5){
+                index++
+                timeStamp = timerCompardo
+            }
+            if (index > mensagem.length){
+                index = 0
+            }
+
+            return mensagem[index]
+
+        case 'nomeCompleto':
+            if (Regras['regrasNome'] === 'regra 0'){
+                return 'Este campo é obrigatorio.'
+            }
+            else if (Regras['regrasNome'] === 'regra 1'){
+                return 'por favor preencha este campo com pelo menos 1 nome e 1 sobrenome.'
+            }
+            else if (Regras['regrasNome'] === 'regra 2'){
+                return 'por favor complemente com 1 sobrenome.'
+            }
+            else {
+                return 'Mensagem generica 1'
+            }
+
+        case 'cpf':
+            if (Regras['regrasCPF'] === 'regra 0'){
+                return 'Este campo é obrigatorio.'
+            }
+            else if (Regras['regrasCPF'] === 'regra 1'){
+                return 'Por favor Digite os 11 digitos do CPF'
+            }
+            else {
+                return 'Mensagem generica 2'
+            }
+
+        case 'email':
+            if (Regras['regrasEmail'] === 'regra 0'){
+                return 'por favor insira uma terminação valida'
+            }
+            else if (Regras['regrasEmail'] === 'regra 1'){
+                return 'por favor insira um terminação'
+            }
+            else if (Regras['regrasEmail'] === 'regra 2'){
+                return 'por favor insira um dominio valido'
+            }
+            else if (Regras['regrasEmail'] === 'regra 3'){
+                return 'por favor insira um usuario valido.'
+            }
+            else 'mensagem generica 3'
+
+        case 'telefone':
+            if (Regras['regrasTelefone'] === 'regra 0') {
+                return `Por favor digite TODOS os digitos do DDD e do TELEFONE FIXO`
+            }
+            else {
+                return 'mensagem generica 4'
+            }
+
+        case 'celular':
+            if (Regras['regrasTelefone'] === 'regra 1') {
+                return `Por favor digite TODOS os digitos do DDD e do CELULAR`
+            }
+            else {
+                return 'mensagem generica 5'
+            }
+
+        case 'cep':
+            if (Regras['regrasCEP'] === 'regra 0'){
+                return 'Por favor digite um CEP valido, para preenchimento automatico do endereço.'
+            }
+            else if (Regras['regrasCEP'] === 'regra 1'){
+                return 'o CEP precisa ter 8 digitos.'
+            }
+            else if (Regras['regrasCEP'] === 'regra 2'){
+                return 'CEP ineserido não está registrado no banco de dados.'
+            }
+            else{
+                return 'mensagem generica 6'
+            }
+
+        case 'endereco':
+            if (Regras['regrasEndereco'] === 'regra 0'){
+                return `Por favor insira o endereço nessa ordem: LOGRADOUR, BAIRRO, LOCALIDADE, UF, PAÍS.`
+            }
+            else {
+                return 'mensagem generica 7'
+            }
+
+        case 'complemento':
+            var mensagem = ['mensagem 1', 'mensagem 2', 'mensagem 3', 'mensagem 4', 'mensagem 5']
+            
+            if (timeStamp <= timerCompardo - 5){
+                index++
+                timeStamp = timerCompardo
+            }
+            if (index > mensagem.length){
+                index = 0
+            }
+
+            return mensagem[index]
+
+        default:
+            console.error('"tipo" invalido!');
+    }
+}
+
 
 //Função exclusiva para o nome completo
 function validarNome(objeto) {
     var nome = objeto.val().split(' ')
-
-    if (nome.length < 2){
+    if (nome[0] == ''){
+        Regras['regrasNome'] = 'regra 0'
+        return false
+    }
+    else if(nome.length < 2){
+        Regras['regrasNome'] = 'regra 1'
         return false
     }
     else {
         if (nome[1] == ''){
+            Regras['regrasNome'] = 'regra 2'
             return false
         }
+        Regras['regrasNome'] = ''
         return true
     }
 }
 
 //função exclusiva para o CPF
 function validarCPF(objeto) {
-    if (objeto.val().length !== 14) {
+    if (objeto.val().length <= 1){
+        Regras['regrasCPF'] = 'regra 0'
+    }
+    else if (objeto.val().length !== 14) {
+        Regras['regrasCPF'] = 'regra 1'
         return false
     }
     else{
+        Regras['regrasCPF'] = ''
         return true
     }
 }
@@ -244,18 +380,23 @@ function validarEmail(objeto) {
         if (indicePonto !== -1 && indicePonto > indiceArroba + 1) {
             if (valor[indicePonto + 1] !== undefined) {
                 if (valor[indicePonto + 1] !== '') {
+                    Regras['regrasEmail'] = ''
                     return true
                 }
                 else {
+                    Regras['regrasEmail'] = 'regra 0'
                     return false
                 }
             } else {
+                Regras['regrasEmail'] = 'regra 1'
                 return false
             }
         } else {
+            Regras['regrasEmail'] = 'regra 2'
             return false
         }
     } else {
+        Regras['regrasEmail'] = 'regra 3'
         return false
     }
 }
@@ -266,18 +407,22 @@ function validarTel(objeto, bool=false) {
     if (bool){
         var tamanho = objeto.val().length
         if (tamanho != 15){
+            Regras['regrasTelefone'] = 'regra 0'
             return false
         }
         else{
+            Regras['regrasTelefone'] = ''
             return true
         }
     }
     else {
         var tamanho = objeto.val().length
         if (tamanho != 17){
+            Regras['regrasTelefone'] = 'regra 1'
             return false
         }
         else {
+            Regras['regrasTelefone'] = ''
             return true
         }
     }
@@ -286,16 +431,23 @@ function validarTel(objeto, bool=false) {
 //funções exclusivas para o cep
 function validarCEP(objeto) {
     var tamanho = objeto.val().length
-    if (tamanho != 10){
+    if (tamanho <= 5) {
+        Regras['regrasCEP'] = 'regra 0'
+        return false
+    }
+    else if (tamanho != 10){
+        Regras['regrasCEP'] = 'regra 1'
         return false
     }
     else{
         var cepLimpo = objeto.val().replace(/\D/g, '')
 
         if (verificarCEP(cepLimpo)){
+            Regras['regrasCEP'] = ''
             return true
         }
         else{
+            Regras['regrasCEP'] = 'regra 2'
             ativarEndereco()
             return false
         }
@@ -355,10 +507,12 @@ function escreverEndereco(bool, data){
 
 function validarEnd(objeto){
     var endereco = objeto.val().split(' ')
-    if (endereco.length < 4){
+    if (endereco.length < 5){
+        Regras['regrasEndereco'] = 'regra 0'
         return false
     }
     else {
+        Regras['regrasEndereco'] = ''
         return true
     }
 }
@@ -372,12 +526,6 @@ function validarComp(objeto){
         return false
     }
 }
-
-
-
-
-
-
 
 
 
